f0471349d265c04a19dc73d717131b413dfa59a6
onmygold <499452621@qq.com>
2017-06-15 08:55:32 +0800

    aa

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Home extends React.Component{
+  render(){
+    return(
+      <div className='home'>
+        <div className='home-cover'>
+          <div>
+            <h1>I'M ALEX </h1>
+            <p> FrontEnd Web Developer </p>
+            <a href='https://github.com/onmygold'>MY GITHUB</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import App from './App.js';
-
-render(<App />,document.getElementById('root'));
+import Routers from './routes';
+import 'highlight.js/styles/atom-one-light.css';
+import './main.css';
+import './post.css';
+render(<Routers />,document.getElementById('root'));
