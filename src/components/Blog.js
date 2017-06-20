6d6a480f3836debc4a3f820fec75c552d77270cc
ohmygold <499452621@qq.com>
2017-06-20 17:29:37 +0800

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -10,7 +10,7 @@ class Blog extends React.Component{
     }
   }
   componentDidMount(){
-    axios.get('https://raw.githubusercontent.com/newming/myblog/master/blogs/blogs.json')
+    axios.get('https://raw.githubusercontent.com/alex/myblog/master/blogs/blogs.json')
       .then( res => this.setState({data: res.data}) )
   }
   render(){
